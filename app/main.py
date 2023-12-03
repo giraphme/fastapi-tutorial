cddae5b71842793521e0b55c159736f4e21f416a
TamakiMaeda <giraph.me@gmail.com>
2023-12-03 21:58:38 +0900

    add /users/{user_id}

--- app/main.py
+++ app/main.py
@@ -11,3 +11,13 @@ async def root():
 @app.get("/items/{item_id}")
 async def get_item(item_id: int):
     return {"item_id": item_id}
+
+
+@app.get("/users/me")
+async def get_user_me():
+    return {"user_id": 0}
+
+
+@app.get("/users/{user_id}")
+async def get_user(user_id: int):
+    return {"user_id": user_id}
