9166128c54ebbcb50a915dc22078104967b99797
TamakiMaeda <giraph.me@gmail.com>
2023-12-09 21:36:36 +0900

    Enum のサンプルを追加

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -26,3 +27,19 @@ async def get_user(user_id: int):
 @app.get("/users/{user_id}/items/{item_id}")
 async def get_user_item(user_id: int, item_id: int, skip: int = 0, limit: int | None = None):
     return {"user_id": user_id, "item_id": item_id, "skip": skip, "limit": limit}
+
+
+class ModelName(str, Enum):
+    alexnet = "Alexnet"
+    resnet = "Resnet"
+    lenet = "Lenet"
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "model": ModelName.alexnet, "message": "Deep Learning FTW!"}
+    if model_name is ModelName.resnet:
+        return {"model_name": model_name, "model": ModelName.resnet, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "model": ModelName.lenet, "message": "Have some residuals"}
